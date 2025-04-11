1e542247c7099eb729d1f4a0c336174371b9471f
kyongwoo <rineee@easymedia.net>
2025-04-11 11:17:51 +0900

    [Stack] 메서드 추가 및 개선사항 수정

--- src/Copystack.java
+++ src/Copystack.java
@@ -1,7 +1,3 @@
-
-import java.util.ArrayList;
-import java.util.Stack;
-
 public class Copystack {
 
     // last in first out
@@ -30,9 +26,13 @@ public class Copystack {
 
 
     public void lengthcheck() {
-        if (idx > intArr.length - 1) {
+        // 같을경우 java.lang.ArrayIndexOutOfBoundsException 발생하므로 = 부등호 추가
+        if (idx >= intArr.length - 1) {
             tmpArr = intArr;
-            intArr = new int[intArr.length + 5];
+
+            // 개선사항2 : 배열을 동적으로 늘려줄 경우 현재 배열 길이의 2배씩 늘려주는게
+            // 재할당과 복사의 횟수를 줄일 수 있다.
+            intArr = new int[intArr.length * 2];
             for (int num = 0 ; num < tmpArr.length; num++) {
                 intArr[num] = tmpArr[num];
             }
@@ -41,7 +41,36 @@ public class Copystack {
     }
 
     public int size() {
-        return idx;
+        // size는 현재 Stack의 크기를 리턴해줘야 함
+        return idx + 1;
     }
 
+
+    //pick 현재 가장 위에 있는 값을 리턴해줌
+    public int peek() {
+        // 개선사항1 : peek 메서드에서 예외처리 필요
+        if (empty()) {
+            throw new RuntimeException("스택이 부족합니다");
+        }
+        return intArr[idx];
+    }
+
+    //empty
+    public boolean empty() {
+        return idx == -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CopyStack : [");
+        for (int i = 0; i <= idx; i++) {
+            if (i == idx) {
+                sb.append(intArr[i] + "]");
+            } else {
+                sb.append(intArr[i] + ",");
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/UseCopyStack.java
+++ src/UseCopyStack.java
@@ -4,21 +4,20 @@ public class UseCopyStack {
 
     public static void main(String[] args) {
         Copystack c = new Copystack();
-
+        System.out.println(c.empty());
         c.push(1);
         c.push(2);
         c.push(3);
         c.push(4);
-        System.out.println(c.pop());
-        System.out.println(c.pop());
-        System.out.println(c.pop());
-        System.out.println(c.pop());
         c.push(7);
         c.push(8);
         c.push(9);
         c.push(3);
-        System.out.println(c.pop());
-        System.out.println("C의 size : " + c.size());
+
+        System.out.println(c.peek());
+        System.out.println(c.empty());
+        System.out.println(c);
+        System.out.println(c.toString());
     }
 }
 
